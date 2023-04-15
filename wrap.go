4978093462a08faad40917659ae32fe2e814db63
Andrew LeFevre <jalefevre@liberty.edu>
2023-04-15 17:56:23 -0400

    flush nftables changes in small batches instead of all at once (#81)
    This will make future debugging easier, as now an nftables error will be
    related to a specific set of nftables operations, instead of all the
    nftables operations for a container. Also log errors when creating
    non-essential rules and continue instead of stopping rule creation
    entirely.
    
    Updates #71.

--- wrap.go
+++ wrap.go
@@ -37,19 +37,3 @@ func (w *wrappedDockerClient) ContainerInspect(ctx context.Context, containerID 
 		return w.dockerClient.ContainerInspect(ctx, containerID)
 	})
 }
-
-// withTimeout runs f with a timeout derived from [context.WithTimeout].
-// Using withTimeout guarantees that:
-//
-//   - ctx is only shadowed in withTimeout's scope
-//   - The child context will have it's resources released immediately
-//     after f returns
-//
-// The main goal of withTimeout is to prevent shadowing ctx with a
-// context with a timeout, having that timeout expire and the next call
-// that uses ctx immediately fail.
-func withTimeout[T, E any](ctx context.Context, timeout time.Duration, f func(ctx context.Context) (T, E)) (T, E) {
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-	return f(ctx)
-}
